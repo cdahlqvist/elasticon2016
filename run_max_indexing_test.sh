9e35d41558927b0730b6cbf0efdc81285103666c
Christian Dahlqvist <christian@elastic.com>
2016-01-25 15:55:56 +0000

    Initial version.

---
+++ run_max_indexing_test.sh
@@ -0,0 +1,4 @@
+$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_wn_724_index_max.json -i 0 -d 2
+$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_nn_724_index_max.json -i 0 -d 2
+$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_wn_286_index_max.json -i 0 -d 2
+$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_nn_286_index_max.json -i 0 -d 2
