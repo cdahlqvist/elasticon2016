14ce9d77fd443db47d7061d764244e6ffee107ed
Christian Dahlqvist <christian@elastic.com>
2016-01-25 23:36:55 +0000

    Initial version

--- run_full_shard_sizing_test_1.sh
+++ run_full_shard_sizing_test_1.sh
@@ -2,9 +2,7 @@
 echo "Start shard sizing tests"
 
 TIMESTAMP=$(date +%s)
-./run_single_shard_sizing_test.sh shard_wn_724 $TIMESTAMP 35
-./run_single_shard_sizing_test.sh shard_nn_724 $TIMESTAMP 35
-./run_single_shard_sizing_test.sh shard_wn_286 $TIMESTAMP 60
-./run_single_shard_sizing_test.sh shard_nn_286 $TIMESTAMP 60
+./run_single_shard_sizing_test_1.sh shard_wn_724 $TIMESTAMP 35
+./run_single_shard_sizing_test_1.sh shard_nn_724 $TIMESTAMP 35
 
 echo "Completed shard sizing tests"
--- run_single_shard_sizing_test_1.sh
+++ run_single_shard_sizing_test_1.sh
@@ -5,13 +5,13 @@ RUNTIMESTAMP=$2
 ITERATIONS=$3
 RUNID="$RUNTIMESTAMP_$INDEXNAME"
 
-curl -XDELETE https://$ES_HOST/shard* -u $ES_USER:$ES_PASSWORD
+curl -XDELETE https://$ES_HOST1/shard* -u $ES_USER:$ES_PASSWORD
 
 for (( i = 1; i <= $ITERATIONS; i+= 1 ))
 do
-  $RANKIN_PATH/bin/rankin -h $ES_HOST -c $ES_USER:$ES_PASSWORD -r $RUNID_index -i 0 -d 10 -f ./configs/$INDEXNAME_index_2.5k.json -a 2
+  $RANKIN_PATH/bin/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -r $RUNID_index -i 0 -d 10 -f ./configs/$INDEXNAME_index_2.5k.json -a 2
   
-  curl -XDELETE https://$ES_HOST/shard*/_optimize -u $ES_USER:$ES_PASSWORD
+  curl -XDELETE https://$ES_HOST1/shard*/_optimize -u $ES_USER:$ES_PASSWORD
 
-  $RANKIN_PATH/bin/rankin -h $ES_HOST -c $ES_USER:$ES_PASSWORD -r $RUNID_query -i 0 -d 3 -f ./configs/$INDEXNAME_query.json -f ./configs/stats_retrieval.json -a 2
+  $RANKIN_PATH/bin/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -r $RUNID_query -i 0 -d 3 -f ./configs/$INDEXNAME_query.json -f ./configs/stats_retrieval.json -a 2
 done
