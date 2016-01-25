f55bfc6e3bbb9f15f3980320a61ade153585b017
Christian Dahlqvist <christian@elastic.com>
2016-01-25 23:46:06 +0000

    Initial version

--- run_single_shard_sizing_test_2.sh
+++ run_single_shard_sizing_test_2.sh
@@ -9,9 +9,9 @@ curl -XDELETE https://$ES_HOST2/shard* -u $ES_USER:$ES_PASSWORD
 
 for (( i = 1; i <= $ITERATIONS; i+= 1 ))
 do
-  $RANKIN_PATH/bin/rankin -h $ES_HOST2 -c $ES_USER:$ES_PASSWORD -r $RUNID_index -i 0 -d 10 -f ./configs/$INDEXNAME_index_2.5k.json -a 2
+  $RANKIN_PATH/rankin -h $ES_HOST2 -c $ES_USER:$ES_PASSWORD -r $RUNID_index -i 0 -d 10 -f ./configs/$INDEXNAME_index_2.5k.json -a 2
   
   curl -XDELETE https://$ES_HOST2/shard*/_optimize -u $ES_USER:$ES_PASSWORD
 
-  $RANKIN_PATH/bin/rankin -h $ES_HOST2 -c $ES_USER:$ES_PASSWORD -r $RUNID_query -i 0 -d 3 -f ./configs/$INDEXNAME_query.json -f ./configs/stats_retrieval.json -a 2
+  $RANKIN_PATH/rankin -h $ES_HOST2 -c $ES_USER:$ES_PASSWORD -r $RUNID_query -i 0 -d 3 -f ./configs/$INDEXNAME_query.json -f ./configs/stats_retrieval.json -a 2
 done
