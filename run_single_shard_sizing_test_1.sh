137bc66c0960ef90d082c3d3c36b6259822af160
Christian Dahlqvist <christian@elastic.com>
2016-01-26 09:33:51 +0000

    Initial version

--- run_single_shard_sizing_test_1.sh
+++ run_single_shard_sizing_test_1.sh
@@ -11,7 +11,7 @@ echo ""
 
 for (( i = 1; i <= $ITERATIONS; i+= 1 ))
 do
-  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -p https -r $RUNID\_index -i 0 -d 10 -f ./configs/$INDEXNAME\_index_2_5k.json -f ./config/cluster.json  -a 2  -D ./results
+  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -p https -r $RUNID\_index -i 0 -d 10 -f ./configs/$INDEXNAME\_index_2_5k.json -f ./configs/cluster.json  -a 2  -D ./results
   
   curl https://$ES_HOST1/shard*/_optimize -u $ES_USER:$ES_PASSWORD
 
@@ -19,5 +19,5 @@ do
 
   sleep 5
 
-  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -p https -r $RUNID\_query -i 0 -d 3 -f ./configs/$INDEXNAME\_query.json -f ./configs/stats_retrieval.json  -f ./config/cluster.json -a 2  -D ./results
+  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -p https -r $RUNID\_query -i 0 -d 3 -f ./configs/$INDEXNAME\_query.json -f ./configs/stats_retrieval.json  -f ./configs/cluster.json -a 2  -D ./results
 done
