57f94a9730f370fc71275a1bda863588a0a24780
Christian Dahlqvist <christian@elastic.com>
2016-01-26 07:55:22 +0000

    Initial version.

--- index_templates/load_index_templates.sh
+++ index_templates/load_index_templates.sh
@@ -1,2 +1,4 @@
-curl -XPOST https://$ES_HOST/_template/shard_wn -u $ES_USER:$ES_PASSWORD -d @shard_wn.json
-curl -XPOST https://$ES_HOST/_template/shard_nn -u $ES_USER:$ES_PASSWORD -d @shard_nn.json
+curl -XPOST https://$ES_HOST1/_template/shard_wn -u $ES_USER:$ES_PASSWORD -d @shard_wn.json
+curl -XPOST https://$ES_HOST1/_template/shard_nn -u $ES_USER:$ES_PASSWORD -d @shard_nn.json
+curl -XPOST https://$ES_HOST2/_template/shard_wn -u $ES_USER:$ES_PASSWORD -d @shard_wn.json
+curl -XPOST https://$ES_HOST2/_template/shard_nn -u $ES_USER:$ES_PASSWORD -d @shard_nn.json
--- run_single_shard_sizing_test_1.sh
+++ run_single_shard_sizing_test_1.sh
@@ -9,9 +9,9 @@ curl -XDELETE https://$ES_HOST1/shard* -u $ES_USER:$ES_PASSWORD
 
 for (( i = 1; i <= $ITERATIONS; i+= 1 ))
 do
-  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -r $RUNID_index -i 0 -d 10 -f ./configs/$INDEXNAME_index_2_5k.json -a 2
+  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -p https -r $RUNID\_index -i 0 -d 10 -f ./configs/$INDEXNAME\_index_2_5k.json -a 2  -D ./results
   
   curl -XDELETE https://$ES_HOST1/shard*/_optimize -u $ES_USER:$ES_PASSWORD
 
-  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -r $RUNID_query -i 0 -d 3 -f ./configs/$INDEXNAME_query.json -f ./configs/stats_retrieval.json -a 2
+  $RANKIN_PATH/rankin -h $ES_HOST1 -c $ES_USER:$ES_PASSWORD -p https -r $RUNID\_query -i 0 -d 3 -f ./configs/$INDEXNAME\_query.json -f ./configs/stats_retrieval.json -a 2  -D ./results
 done
