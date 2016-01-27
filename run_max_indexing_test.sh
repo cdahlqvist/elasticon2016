806fd0acdc7ca64b2d1b97ee3528bd6bbfa37c7a
Christian Dahlqvist <christian@elastic.com>
2016-01-27 16:00:57 +0000

    Initial version

--- run_max_indexing_test.sh
+++ run_max_indexing_test.sh
@@ -1,4 +1,4 @@
-$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_wn_724_index_max.json -i 0 -d 2
-$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_nn_724_index_max.json -i 0 -d 2
-$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_wn_286_index_max.json -i 0 -d 2
-$RANKIN_PATH/rankin -D ./results -r max_indexing_test_500_4 -f ./configs/shard_nn_286_index_max.json -i 0 -d 2
+$RANKIN_PATH/rankin -h $ES_HOST -D ./results -r max_indexing_test_500_4 -c $ES_USER:$ES_PASSWORD -p https -f ./configs/shard_wn_724_index_max.json -i 0 -d 2
+$RANKIN_PATH/rankin -h $ES_HOST -D ./results -r max_indexing_test_500_4 -c $ES_USER:$ES_PASSWORD -p https -f ./configs/shard_nn_724_index_max.json -i 0 -d 2
+$RANKIN_PATH/rankin -h $ES_HOST -D ./results -r max_indexing_test_500_4 -c $ES_USER:$ES_PASSWORD -p https -f ./configs/shard_wn_286_index_max.json -i 0 -d 2
+$RANKIN_PATH/rankin -h $ES_HOST -D ./results -r max_indexing_test_500_4 -c $ES_USER:$ES_PASSWORD -p https -f ./configs/shard_nn_286_index_max.json -i 0 -d 2
